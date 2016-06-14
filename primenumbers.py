26246092efe36ad7e5e069192eef2762a29906c5
mercie wafula <mrcwafula@gmail.com>
2016-06-14 18:56:16 +0300

    Added Prime Numbers

---
+++ primenumbers.py
@@ -0,0 +1,20 @@
+def primenumbers(n):
+
+	primelist=[2,3]
+
+	current_no=4
+
+	while True:
+		for i in range(2,(current_no//2)+1):
+
+			if current_no % i==0:
+				break
+		else:
+			primelist.append(current_no)
+		current_no+=1
+
+		if len(primelist)==n:
+			break
+	return primelist
+
+print(primenumbers(8))
